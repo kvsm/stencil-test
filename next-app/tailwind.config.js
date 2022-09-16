c3efbf265852381377d6938708873b1c71ca1734
Kev Smith <kevin.smith@sds.co.uk>
2022-09-16 11:53:23 +0100

    Add library to tailwind content

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/stencil-library/src/components/**/*.tsx"
   ],
   theme: {
     extend: {},
